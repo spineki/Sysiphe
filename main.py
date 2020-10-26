bf56e4868b4997a41db34b7007f835855b6819aa
Marras Antoine <antoine.marras@student-cs.fr>
2020-10-26 11:55:33 +0100

    fully functionnal simple python file

--- main.py
+++ main.py
@@ -32,6 +32,13 @@ def takeScreenShot():
                         cv2.COLOR_RGB2BGR)
 
 
+def display_contour_on_img(box, img):
+    display_img = img.copy()
+    cv2.drawContours(display_img, [box], -1, (0, 255, 0), 3)
+    plt.imshow(display_img)
+    plt.show()
+
+
 def adaptImage(img, display=False):
     # Denoising the picture
     img_denoised = cv2.fastNlMeansDenoisingColored(img, None, 20, 20, 7, 21)
@@ -77,7 +84,38 @@ def findProgressBar(img):
             new_contours.append(cnt)
             break
 
-    assert len(new_contours) == 1, "more thant one contour found: found %r" % len(
+    assert len(new_contours) == 1, "more thant one contour for progressbar found: found %r" % len(
+        contours)
+
+    return new_contours[0]
+
+
+def findCuphead(img):
+    contours, hierarchy = cv2.findContours(
+        img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    new_contours = []
+
+    i = -1
+    for cnt in contours:
+        i += 1
+        # I have used min Area rect for better result
+        rect = cv2.minAreaRect(cnt)
+        width = rect[1][0]
+        height = rect[1][1]
+        area = width * height
+        if height == 0 or width == 0:
+            continue
+        ratio = width/height
+        if abs(ratio - CUPHEAD_RATIO) < EPSILON_CUPHEAD_RATIO and abs(area - CUPHEAD_AREA) < EPSILON_CUPHEAD_AREA:
+            _, box = get_box_from_contour(contours[i])
+            new_contours.append(cnt)
+            break
+
+    print("nombres de contours trouvés ", len(new_contours))
+
+    assert len(new_contours) == 1, "more thant one contour for cuphead found: found %r" % len(
         contours)
 
     return new_contours[0]
@@ -113,7 +151,34 @@ if __name__ == "__main__":
         plt.imshow(mask)
         plt.show()
 
+    # we get the contour of the progressbar
     progressbar_contour = findProgressBar(mask)
 
+    # we extract the bounding box from it
     progressbar_rect, progressbar_box = get_box_from_contour(
         progressbar_contour)
+
+    if display:
+        display_contour_on_img(progressbar_box, origin_img)
+
+    # no need to work with the full picture, we can safely crop the progressbar
+    x, y, w, h = cv2.boundingRect(progressbar_contour)
+    cropped_progressbar = mask[y:y+h, x:x+w]
+    if display:
+        plt.imshow(cropped_progressbar, cmap='gray')
+        plt.show()
+
+    # we get the contour of cuphead
+    cuphead_contour = findCuphead(cropped_progressbar)
+
+    # We extract the bounding box from it
+    cuphead_rect, cuphead_box = get_box_from_contour(cuphead_contour)
+    if display:
+        display_contour_on_img(cuphead_box, origin_img)
+
+    progressbar_w = progressbar_rect[1][0]
+    cuphead_x = cuphead_rect[0][0]
+
+    relative_progress = cuphead_x / progressbar_w
+
+    print(int(relative_progress * 100), "%")
